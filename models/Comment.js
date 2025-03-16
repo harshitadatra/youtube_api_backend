d7939e43285f4bb127243c831ee8e36b6df6d477
harshitadatra <harshitkiran22@gmail.com>
2025-03-16 12:40:15 +0530

    feat: added comment schema

---
+++ models/Comment.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const CommentSchema = new mongoose.Schema({
+  videoId: String,
+  text: String,
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
