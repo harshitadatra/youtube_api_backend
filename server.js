94d160846fa91511719b976007db047f07143693
harshitadatra <harshitkiran22@gmail.com>
2025-03-24 02:58:51 +0530

    fix: added allowedOrigins

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
 dotenv.config();
+const allowedOrigins = [
+  "http://localhost:3000", // Localhost
+  process.env.FRONTEND_URL,// Deployed frontend
+  "http://localhost:5000"
+];
+
 
 const videoRoutes = require("./routes/videoRoutes");
 const commentRoutes = require("./routes/commentRoutes");
@@ -12,6 +18,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 console.log('MongoDB URI:', process.env.MONGO_URI);
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true // Allow cookies (if needed)
+}));
+
 
 
 mongoose.connect(process.env.MONGO_URI, {
